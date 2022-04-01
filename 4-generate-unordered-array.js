f54b891a0a97809bc22b36c146a23a149f6e8d60
Mahefa Ny Anjara <98638690+Mahefaa@users.noreply.github.com>
2022-04-01 16:54:30 +0300

    Update 4-generate-unordered-array.js

--- 4-generate-unordered-array.js
+++ 4-generate-unordered-array.js
@@ -1,15 +1,18 @@
-const { arrayContain } = require('./3-array-contain');
+const { arrayContain } = require('./3-array-contain');//1+1=2
 
 function generateUnorderedArray(size) {
-  let array = [];
-  for (let i = 0; i < size; i++) {
-    let number = Math.floor(Math.random() * 100);
-    while (arrayContain(array, number)) {
-      number = Math.floor(Math.random() * 100);
+  let array = [];//1
+  for (let i = 0; i < size; i++) {//(1+1+1+1+1)*n=5n
+    let number = Math.floor(Math.random() * 100);//1+1+1+1=4
+    while (arrayContain(array, number)) {//(1+1)*n=2n
+      number = Math.floor(Math.random() * 100);//1+1+1+1=4
     }
-    array.push(number);
+    array.push(number);//1+1=2
   }
-  return array;
+  return array;//1
 }
-
+/**
+Complexité dans le pire des cas T(n)=O(1)+O(1)+O(5n(4+2n(4)+2)+O(1) peut être simplifié en n^2
+donc T(n)=O(n^2) complexité quadratique
+**/
 module.exports = { generateUnorderedArray };
