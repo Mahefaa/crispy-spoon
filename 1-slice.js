1666806988b24700e0b9e0ffbe9ff4ecb78fed40
Mahefa Ny Anjara <98638690+Mahefaa@users.noreply.github.com>
2022-04-01 16:24:55 +0300

    Update 1-slice.js

--- 1-slice.js
+++ 1-slice.js
@@ -1,9 +1,9 @@
 function slice(array, start, end) {
   let slicedArray = []; //1
-  for (let i = start; i < end; i++) { //(1+1+1+1+1+1)*n
-    slicedArray.push(array[i]);//1+1
+  for (let i = start; i < end; i++) { //(1+1+1+1+1+1)*n=6n
+    slicedArray.push(array[i]);//1+1=2
   }
-  return slicedArray;//1
+  return slicedArray;//1=1
 }
 /** T(n)=O(1)+O(6n*2)+O(n)
 avec lim (T(n) n->+infini ) == lim O(6n*2) == lim O(n) =>T(n)=O(n)
